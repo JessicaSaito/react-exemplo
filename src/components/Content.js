dfdc6f037efb282158ce5ad94cdafc0fd8d613c9
JESSICA <cg3021751@LAB204M14.IFSP>
2023-03-09 17:57:49 -0300

    Criação de Alert

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,35 @@
-import Box from '@mui/material/Box';
+import Box from '@mui/material/Box'
+import Alert from './Alert'
 
 const Content = () => {
+
     return (
         <Box sx={{
-            background: '#66CDAA',
-            width: '1500px',
-            height: '500px'
+            background: "#EEE",
+            width: 500,
+            height: 500,
+            padding: '15px'
         }}>
-            <h2>Content</h2>
+            <h1>Content</h1>
+            
+                <Alert type="success" >
+                    Deu certo!
+                </Alert>
+
+                <Alert type="error">
+                    Deu Erro!
+                </Alert>
+
+                <Alert type="info">
+                    Info!
+                </Alert>
+
+                <Alert type="warning">
+                   Warning!
+                </Alert>   
+            
         </Box>
     )
-  }
-  
-  export default Content
+}
+
+export default Content
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -54,6 +54,57 @@ const Sidebar = () => {
             }} />
         </Box>
     )
+
+    {/*
+        return (
+        <Box sx={{
+            background: "#DDD",
+            width: 200,
+            height: 500,
+            padding: '15px'
+        }}>
+            <h2>Sidebar</h2>
+            <img 
+                style={{
+                    width: 200,
+                    height: 123.83
+                }}
+                src="https://i.natgeofe.com/k/830b5d15-92db-429f-a80a-cc89b5700af5/mt-everest.jpg?w=500"
+                alt="Mountain"
+            />
+            <hr />
+            <img
+                style={{
+                    width: 200,
+                    heigh: 120
+                }}
+                src={fotoMontanha}
+                alt="Mountain"
+            />
+            <hr />
+            <IconCarrinho style={{
+                width: 30,
+                height: 30,
+                fill: '#345345'
+            }} />
+            <hr />
+            <IconCarrinhoMais 
+            style={{
+                width: 40,
+                height: 40,
+                color: '#345345'
+            }}
+            /><hr />
+            <IconCarrinho style={{
+                width: 30,
+                height: 30,
+                fill: '#982234'
+            }} />
+            <IconOutroCarrinho />
+            <CarrinhoRoda />        
+        </Box>
+    )
+    */}
 }
 
 export default Sidebar

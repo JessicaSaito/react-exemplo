e5b3c3bc30b312edb3ee1573139c84a694fceb71
JessicaSaito <jsayu.saito@gmail.com>
2023-03-17 13:44:36 -0300

    Arrumando imagens

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import Box from '@mui/material/Box';
 import fotoNina from './assets/img/fotoNina.jpg';
-import { ReactComponent as IconNinja } from './assets/img/ninjas.svg';
+import { ReactComponent as IconCarrinho } from './assets/img/carrinho.svg'
 import {
     FaCartPlus as IconCarrinhoMais,
     FaLuggageCart as IconOutroCarrinho
-} from 'react-icons/fa'
-import { FaCartPlus as IconCarrinhoMais } from 'react-icons/gi'
+} from 'react-icons/fa';
+import { GiCartwheel as CarrinhoRoda } from 'react-icons/gi'
+
 
 const Sidebar = () => {
     return (
@@ -16,55 +17,7 @@ const Sidebar = () => {
             padding: '15px'
         }}>
             <h2>Sidebar</h2>
-            <hr />
-
-            <img style={{
-                width: 180,
-                height: '123.83px'
-            }}
-                scr="" alt="" />
-            {/* {colocar url da imagem, problema de img na url, se a imagem sumir, o site não exibira ela} */}
-
-            <img style={{
-                width: 180,
-                height: '123.83px'
-            }}
-                scr={fotoNina}
-                alt="foto gata branca com manchas preta, chamada Nina" />
-            <hr />
-
-            <IconNinja style={{
-                width: 30,
-                height: 30,
-                color: '#345345'
-                /*{ provavelmente nao vai mudar a cor, daria mudar direto no svg no fill e forcar perder a cor padrao}*/
-            }} />
-            <hr />
-
-            <IconCarrinhoMais style={{
-                width: 30,
-                height: 30,
-                color: '#345345'
-            }} />
-            <hr />
-
-            <IconOutroCarrinho style={{
-                width: 30,
-                height: 30
-            }} />
-        </Box>
-    )
-
-    {/*
-        return (
-        <Box sx={{
-            background: "#DDD",
-            width: 200,
-            height: 500,
-            padding: '15px'
-        }}>
-            <h2>Sidebar</h2>
-            <img 
+            <img
                 style={{
                     width: 200,
                     height: 123.83
@@ -73,38 +26,39 @@ const Sidebar = () => {
                 alt="Mountain"
             />
             <hr />
-            <img
-                style={{
-                    width: 200,
-                    heigh: 120
-                }}
-                src={fotoMontanha}
-                alt="Mountain"
-            />
+
+            <img style={{
+                width: 180,
+                height: '123.83px'
+            }}
+                scr={fotoNina}
+                alt="foto gata branca com manchas pretas, chamada Nina" />
             <hr />
+
             <IconCarrinho style={{
                 width: 30,
                 height: 30,
                 fill: '#345345'
             }} />
             <hr />
-            <IconCarrinhoMais 
-            style={{
-                width: 40,
-                height: 40,
-                color: '#345345'
-            }}
+
+            <IconCarrinhoMais
+                style={{
+                    width: 40,
+                    height: 40,
+                    color: '#345345'
+                }}
             /><hr />
+
             <IconCarrinho style={{
                 width: 30,
                 height: 30,
                 fill: '#982234'
             }} />
             <IconOutroCarrinho />
-            <CarrinhoRoda />        
+            <CarrinhoRoda />
         </Box>
     )
-    */}
 }
 
 export default Sidebar
